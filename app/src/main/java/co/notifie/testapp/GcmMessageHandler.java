29e5c2605d549e0f59320df31d550520b8da7416
bedunkevich <stas.bedunkevich@gmail.com>
2015-04-04 21:26:36 +0300

    notifiaction builder

--- app/src/main/java/co/notifie/testapp/GcmMessageHandler.java
+++ app/src/main/java/co/notifie/testapp/GcmMessageHandler.java
@@ -1,6 +1,8 @@
 package co.notifie.testapp;
 
 import android.app.IntentService;
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
@@ -26,7 +28,7 @@ public class GcmMessageHandler extends IntentService {
         super.onCreate();
         handler = new Handler();
     }
-    
+
     @Override
     protected void onHandleIntent(Intent intent) {
         Bundle extras = intent.getExtras();
@@ -48,6 +50,18 @@ public class GcmMessageHandler extends IntentService {
         handler.post(new Runnable() {
             public void run() {
                 Toast.makeText(getApplicationContext(), mes, Toast.LENGTH_LONG).show();
+
+                Notification noti = new Notification.Builder(getApplicationContext())
+                        .setContentTitle("Notifie")
+                        .setContentText(mes)
+                        .setAutoCancel(true)
+                        .setSmallIcon(R.drawable.ic_launcher)
+                        .build();
+
+                NotificationManager notificationManager =
+                        (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+                notificationManager.notify(0, noti);
             }
         });
 
